c72d8b07289d234edcf3f991a848d0bd1cbfb7ce
Sandeep Rao <sandeepvasudevarao@gmail.com>
2025-08-15 23:00:43 +0530

    bump version

--- internal/core/app.go
+++ internal/core/app.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vrsandeep/mango-go/internal/websocket"
 )
 
-const Version = "0.0.2" // Application version
+const Version = "0.0.3" // Application version
 
 // App holds the core components of the application that are shared
 // between the server and the CLI.
